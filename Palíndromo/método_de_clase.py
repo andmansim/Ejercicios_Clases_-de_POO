80f4fa8c77d4232b22b01684349eb28833ef09fa
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-15 19:40:19 +0100

    terminada primera parte

--- Palíndromo/método_de_clase.py
+++ Palíndromo/método_de_clase.py
@@ -27,12 +27,14 @@ class Palindromo():
             
         # invertir 
         f2 = ""
-        for i in range(0, len(f) - 1):
-            f2 = f[i] + f2
-            # pasa la primera letra al final y la guarda en una nueva cadena            
-
+        for i in range(0, len(f)):
+            f2 = f[i] + f2  # pasa la primera letra al final y la guarda en una nueva cadena     
+     
         #Comparamos la frase inicial con el resultado y decimos si es o no
         if f2 == f:
             return True
         else:
             return False
+
+
+print(Palindromo.esPalindromo("Os ó"))
