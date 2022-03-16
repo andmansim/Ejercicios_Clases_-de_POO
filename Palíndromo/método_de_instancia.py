8fb1bada1e2ea5f86beaf43e8671d18d4bb3cd09
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-16 17:39:53 +0100

    Atributo, constructor y método test

--- Palíndromo/método_de_instancia.py
+++ Palíndromo/método_de_instancia.py
@@ -1,41 +1,46 @@
 #Segundo ejercicio
-class Palindromo():
+class Palindromo:
     
-    def esPalindromo(f):
+    def __init__(self, palabra):
+        self.palabra = palabra
+      
+    def test(self):
         #quitar espacios
-        if " " in f:
-            f = f.replace(" ", "")
+        if " " in self.palabra:
+            self.palabra = self.palabra.replace(" ", "")
 
         #quitar toda parte acentuada
 
-        if "á" in f:    
-            f = f.replace("á", "a")
+        if "á" in self.palabra:    
+            self.palabra = self.palabra.replace("á", "a")
             
-        if "é" in f:
-            f = f.replace('é', 'e')
+        if "é" in self.palabra:
+            self.palabra = self.palabra.replace('é', 'e')
 
-        if "í" in f:
-            f = f.replace('í', 'i')
+        if "í" in self.palabra:
+            self.palabra = self.palabra.replace('í', 'i')
 
-        if "ó" in f:
-            f = f.replace('ó', 'o')
+        if "ó" in self.palabra:
+            self.palabra = self.palabra.replace('ó', 'o')
             
-        if "ú" in f:
-            f = f.replace('ú', 'u')
+        if "ú" in self.palabra:
+            self.palabra = self.palabra.replace('ú', 'u')
 
         #transformar toda letra mayúscula en minúscula
-        f = f.lower()
+        self.palabra = self.palabra.lower()
             
         # invertir 
         f2 = ""
-        for i in range(0, len(f)):
-            f2 = f[i] + f2  # pasa la primera letra al final y la guarda en una nueva cadena     
-     
+        for i in range(0, len(self.palabra)):
+            f2 = self.palabra[i] + f2  # pasa la primera letra al final y la guarda en una nueva cadena     
+        
         #Comparamos la frase inicial con el resultado y decimos si es o no
-        if f2 == f:
+    
+        if f2 == self.palabra:
             return True
         else:
             return False
 
-
-print(Palindromo.esPalindromo("Os ó"))
+#creando instancias
+p = Palindromo("radar")
+print(p.test())
