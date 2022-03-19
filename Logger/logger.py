9453065bb8130f8b109814ef740483ace0e050de
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-19 19:45:19 +0100

    datos logger

---
+++ Logger/logger.py
@@ -0,0 +1,9 @@
+class Logger:
+    #escribe mensaje dado como archivo cada vez que se llame a la funcion log_mensaje.
+    #primera linea archivo: start log
+    #ultima linea de archivo: end log:xlog(s) (cuando se destruye la instancia)
+    #x -> cuantas veces se ha llamado a la funcion log
+
+#llamar a logger en una funcion llamada de una clases Test
+class Test:
+    #esta clase llama a la clase logger con una funcion
