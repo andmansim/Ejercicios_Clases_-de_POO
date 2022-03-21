015ac90b6963cebf3dc36b1930b39bf049c05ca8
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-21 18:08:05 +0100

    codigo principal y prit fichero

--- Logger/logger.py
+++ Logger/logger.py
@@ -6,16 +6,26 @@ class Logger:
     def log_mensaje(self): #esta funcion abre un archivo en el que ecribe el mensaje pasado por parámetro
         f = open("fichero.txt", "w")
         f.write("start log" + '\n')
-        for i in range(0,self.n):
+        for i in range(0,int(self.n)):
             f.write(self.mensaje + '\n')
         f.write("end log:" + str(self.n) + "log(s)")
         f.close()
         return f
 
-hola = Logger("buenas tardes", 4)
-buenas_tardes = "buenas tardes"
-ene = 4
-print(hola.log_mensaje())
+
+print("¿Que mensaje quiere añadir en el archivo?")
+mensaje = input()
+print("¿Cuántas veces lo quiere añadir?")
+ene = int(input())
+mensajito = Logger(mensaje, ene)
+mensajito.log_mensaje()
+f = open("fichero.txt", "r")
+contador = 0
+for line in f:
+    contador = contador + 1
+    if contador <= ene + 2:
+        print(line)
+f.close()
 #llamar a logger en una funcion llamada de una clases Test
 #class Test:
     #esta clase llama a la clase logger con una funcion
