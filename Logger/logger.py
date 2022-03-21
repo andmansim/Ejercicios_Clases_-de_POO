567fc9975ca7c6443f006fa14950eb69551d33c7
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-21 17:52:39 +0100

    arreglo funcion y init

--- Logger/logger.py
+++ Logger/logger.py
@@ -1,22 +1,21 @@
 class Logger:
-    def log_mensaje(mensaje, n): #esta funcion abre un archivo en el que ecribe el mensaje pasado por parámetro
+    def __init__(self, mensaje, n):
+        self.mensaje = mensaje
+        self.n = n
+
+    def log_mensaje(self): #esta funcion abre un archivo en el que ecribe el mensaje pasado por parámetro
         f = open("fichero.txt", "w")
-        f.write("start log")
-        for i in range(1,n):
-            f.write(mensaje + '\n')
-        f.write("end log:" + str(n) + "log(s)")
+        f.write("start log" + '\n')
+        for i in range(0,self.n):
+            f.write(self.mensaje + '\n')
+        f.write("end log:" + str(self.n) + "log(s)")
         f.close()
         return f
 
-    #escribe mensaje dado como archivo cada vez que se llame a la funcion log_mensaje.
-    #primera linea archivo: start log
-    #ultima linea de archivo: end log:xlog(s) (cuando se destruye la instancia)
-    #x -> cuantas veces se ha llamado a la funcion log
-hola = Logger()
+hola = Logger("buenas tardes", 4)
 buenas_tardes = "buenas tardes"
 ene = 4
-fichero = hola.log_mensaje(buenas_tardes, ene)
-print(fichero)
+print(hola.log_mensaje())
 #llamar a logger en una funcion llamada de una clases Test
 #class Test:
     #esta clase llama a la clase logger con una funcion
